65301dd62c9e2f9f7d97458503097900b66741f0
Wherewasjeff <144689068+Wherewasjeff@users.noreply.github.com>
2023-11-07 20:37:26 +0200

    Create main.php

---
+++ main.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="heading.css"></link>
+    <title>Document</title>
+</head>
+<body>
+<div class="main">
+    <div class="head">
+        <div class="hpart">
+            <img src="asset/logo.png" href="cau"</img>
+        </div>
+        <div class="hpart">
+            <a href="main.php" class="butt">Home</a>
+        </div>
+        <div class="hpart">
+            <a href="newtask.php" class="butt">Add Task</a>
+        </div>
+        <div class="hpart">
+            <a href="calendar.php" class="butt">Calendar</a>
+        </div>
+        <div class="hpart">
+            <a href="tasks.php" class="butt">Tasks</a>
+        </div>
+        <div class="hpart">
+            <a href="profile.php" class="butt">Profile</a>
+        </div>
+        <div class="hpart2">
+            <div class="logbutt">
+                <a href="login.php" class="butt">Log In</a>
+            </div>
+        </div>
+        <div class="hpart2">
+            <div class="regbutt">
+                <a href="register.php" class="butt2">Sign Up</a>
+            </div>
+        </div>
+    </div>
+    <h1 class="welcome">Welcome To Tasker!</h1>
+    <p class="text">Our company, Tasker Technologies, was founded on the principle of simplifying project management for individuals and teams alike. We believe that everyone should have access to effective project management tools without the complexity and steep learning curves associated with many other platforms. With Tasker, you'll find an intuitive and feature-rich system that empowers you to create, organize, and complete projects with ease.</p>
+</div>
+</body>
+</html>
